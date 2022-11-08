84af83ec1a8c1c168f3f67d8210ada4b484431c3
Dee Chap <deniseechappell@gmail.com>
2022-11-08 11:09:42 -0600

    Rework Part 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,29 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+//    checks for location input and length
+    @NotBlank(message = "Location required")
+    @Size(min= 1, max = 75)
+    private String location;
 
+//adds public access to employer
+    public Employer () {}
+//gets/sets location from user
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
-
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+@Entity
+//contains no arg constructor
 public class Skill extends AbstractEntity {
+    @NotBlank(message = "Description is required")
+    @Size(max = 100, message = "Description must be less than 100 characters")
+    private String description;
 
+    public Skill () {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
