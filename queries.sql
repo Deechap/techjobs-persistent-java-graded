ecb3f40f6092a67e4a1554d19c746927517a7de8
Dee Chap <deniseechappell@gmail.com>
2022-11-11 12:48:51 -0600

    Part3-4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,18 @@
 -- Part 1: Test it with SQL
-SELECT * FROM jobs;
+SELECT *
+FROM jobs;
+
 -- Part 2: Test it with SQL
+SELECT name
+FROM employer
+WHERE location = "St. Louis City"
 
 -- Part 3: Test it with SQL
+DROP TABLE job;
 
 -- Part 4: Test it with SQL
+SELECT *
+FROM skill
+INNER JOIN job_skills ON skills.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
